be8463d19b0f3f7eba8d4c34fb6cdbd1ebeb58ca
maneeshanif <manees7865@gmail.com>
2024-10-07 21:06:07 +0500

    "🌍"

---
+++ task1/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "../components/about";
+
+const page = () => {
+  return (
+    <div>
+      <About />
+    </div>
+  );
+};
+
+export default page;
---
+++ task1/src/app/components/about.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './about.module.css';
+
+const About = () => {
+  return (
+    <>
+    <section className={style.about}  >
+    <h1> this is About page</h1>
+
+    </section>
+      
+    </>
+  )
+}
+
+export default About
