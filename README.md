6088c070e8fdd15e86ef90b64fdc500a6dda28fe
maneeshanif <manees7865@gmail.com>
2024-10-21 15:32:24 +0500

    "initial commit"

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nextjs-class-assignments
+
+ ok here i will upload my class assignments of quarter 2 in giaic given by class teacher Sir Hamza Alvi 
---
+++ task3/src/components/navbar/navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+const Navbar = () => {
+
+
+  return (
+    <>
+    <div>
+     <Image src="/logo.svg" alt="logo" width={100} height={100} ></Image>
+    </div>
+      
+    </>
+  )
+}
+
+export default Navbar
