f5b049b3588703341b40442affe310cba6e04d34
maneeshanif <manees7865@gmail.com>
2024-10-22 14:40:41 +0500

    "🥲"

--- task3/src/components/navbar/navbar.tsx
+++ task3/src/components/navbar/navbar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Logo from "../../../public/images/nikonlogo.svg"
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-black/60 w-full h- fixed top-0 z-10 ">
+      <nav className="bg-black/60 w-full  fixed top-0 z-10 ">
        <div>
          <Image src={Logo} alt="logo" width={70} height={70} className="mx-[7rem] mt-6 pb-5" />
+       </div>
+       <div className="text-xl text-white  flex  gap-10  px-96   ">
+        <Link href="/" >Business</Link>
+        <Link href="/" >Consumer</Link>
+        <Link href="/" >About Us</Link>
        </div>
       </nav>
     </>
